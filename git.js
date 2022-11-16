e75cd62103cb9c9e7e615f061d09fac791c1bfcc
janardhanRedde <badarijanardhanreddy912@gmail.com>
2022-11-16 17:30:38 +0530

    code

---
+++ git.js
@@ -0,0 +1,14 @@
+//GIT PROCESS TO MOVING FILE FROM VS CODE TO GIT
+
+// 1. open git hub to create a new repo.
+// 2. select public and add read me file to create repository.
+// 3. copy the code on top for reference.
+// 4. then comeback to vs code file & open terminal .
+// 5. type git clone and paste the  repo code link .
+// 6. then type cd .\file name what we created in git\
+// 7. from the top in vs code folder created what you mentioned in git.
+// 8. move the files to that selected folder.
+// 9. in terminal type git add .  
+// 10. in terminal type git status (check either changes or not)
+//11. in terminal type git commit -m "code"
+//12. in terminal type git push
